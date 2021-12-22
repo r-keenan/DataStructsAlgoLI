51c30c1d65eb6f1206f3f0c41a13ceef1195d3a4
Ross Keenan <ross.keenan@protonmail.com>
2021-12-22 11:31:54 -0600

    Stack portion completed. Created queue file.

---
+++ 2 Data Structures/Stack.py
@@ -0,0 +1,15 @@
+# try out the Python stack functions
+
+stack = []
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+print(stack)
+
+x = stack.pop()
+
+print(x)
+print(stack)
